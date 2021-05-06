f01f20ab776db1a2a2dea3e37d27fe01b60dadad
jgoode7 <78943773+jgoode7@users.noreply.github.com>
2021-05-06 16:40:20 -0700

    first upload
    very basic first few consts etc.

---
+++ app.js
@@ -0,0 +1,12 @@
+const qwerty = document.getElementById('#qwerty');
+const phrase = document.getElementById('phrase');
+const btnReset = document.getElementsByClassName('.btn__reset');
+let missed = 0;
+
+let phrases = [
+    'What do you say Billy Ray',
+    'All good in the hood',
+    'Whaddayat',
+    'Oh right on hey?',
+    'Were you born in a barn?'
+];
