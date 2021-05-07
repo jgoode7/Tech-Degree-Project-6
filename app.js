da63650269c1095b29baa2bdda2a7ee6ebc33d67
jgoode7 <78943773+jgoode7@users.noreply.github.com>
2021-05-06 17:29:33 -0700

    got game to launch!
    added code to hide overlay and show keyboard

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+//Const and other variables
 const qwerty = document.getElementById('#qwerty');
 const phrase = document.getElementById('phrase');
-const btnReset = document.getElementsByClassName('.btn__reset');
+const btnReset = document.querySelector('.btn__reset');
+const startGame = document.querySelector('#overlay');
+
+//Variable for wrong answers
 let missed = 0;
 
+//Array for phrases
 let phrases = [
     'What do you say Billy Ray',
     'All good in the hood',
@@ -10,3 +15,9 @@ let phrases = [
     'Oh right on hey?',
     'Were you born in a barn?'
 ];
+
+//Listener to Start Game-- hide overlay start screen
+
+btnReset.addEventListener("click", () => {
+    startGame.style.display = 'none';
+});
